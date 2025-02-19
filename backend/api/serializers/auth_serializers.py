c89c2defbf496e0bfb04dad8656a88df28c38ac6
betancito <jeronimobeta90@gmail.com>
2025-02-19 13:08:10 -0500

    feat: added user type to JWT for frontend routes protection but also did a docs file in order to be able to show a basic view of the endpoints

---
+++ backend/api/serializers/auth_serializers.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomAuthSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        #Custom field for user class
+        token['is_admin'] = user.is_superuser
+        
+        return token
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,20 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.controllers.inspection_controller import InspectionViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 router = DefaultRouter()
 router.register(r'inspections', InspectionViewSet, basename='inspection')
 
 urlpatterns = [
     path('', include(router.urls)),
+    
     #JWTauth endpoints
-    path("token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("token/", views.CustomAuthView.as_view(), name='token_obtain_pair'),
+    
+    #This one is with the purpose of refreshing the token in case is needed by the front for auth reasons
     path("token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    
+    #Path to API basic endpoint documentation
+    path("docs/", views.index)
 ]
